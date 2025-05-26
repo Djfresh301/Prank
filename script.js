66fd6d4a391cbca489a0b34d5aa10041bc920859
Djfresh301 <dillonfindus@gmail.com>
2025-05-26 22:37:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Zeige Fake-Warnung direkt beim Laden
 window.onload = () => {
   const warnung = document.getElementById("virus-warning");
   warnung.style.display = "block";
 
-  // Warnung nach 3 Sekunden automatisch ausblenden
   setTimeout(() => {
     warnung.style.display = "none";
   }, 3000);
